bf1c52c7aa5b6caa661493f4a2ce6c66249ea4d7
owujib <48925577+owujib@users.noreply.github.com>
2021-11-24 03:09:26 +0100

    tested nodemon and fixed script error

--- server/index.js
+++ server/index.js
@@ -1,10 +1,9 @@
-const { application } = require("express");
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
-app.get("/", function (req, res) {
-  res.send("hello world!");
+app.get('/', function (req, res) {
+  res.send('hello world!');
 });
 
 //running server
